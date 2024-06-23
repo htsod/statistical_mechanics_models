c12b325d4f866e393f681802b21e1cae014d6340
dio <tianmingliang@ucsb.edu>
2024-06-23 16:54:21 +0800

    added more info

---
+++ polymers_random_walks/README.md
@@ -0,0 +1,10 @@
+# polymers_random_walks
+
+Modelling the spatial distribution of polymers with restricted random walks.
+
+
+- By simply restricting the random walks not to backtrace leading to different universal class
+
+- RMS distance scale as $\frac{3}{4}$ to the steps size
+
+![graph](./graph.png)
--- polymers_random_walks/main.py
+++ polymers_random_walks/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class grid:
+    # Initialized two dimensional grid
     def __init__(self, size):
         self.size = size
         self.grid = np.zeros([self.size, self.size])
@@ -10,15 +11,24 @@ class grid:
 
 
     def UpdateSteps(self, steps):
+        """
+            Update the steps not allowing walking back
+            return the trajectory
+        """
+
+        # start the walks at the center of the grid
         position_list = [[] for i in range(steps)]
         x, y = int(self.size/2), int(self.size/2)
         position_list[0].append(x)
         position_list[0].append(y)
 
+
         self.grid[x][y] = 0
         past_step = [0, 0]
         for i in range(1, steps):
+            # optinal steps to take in a 2d array
             options = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+            # self-avoiding random walks, no walking back
             if past_step in options:
                 options.remove(past_step)
             step_index = np.random.choice(len(options))
@@ -36,10 +46,10 @@ class grid:
 
 
     def MeasureDistance(self):
+        """
+            Measure the distance away from the initial position
+        """
         return np.sqrt((int(self.size/2) - self.pos[0])**2 + (int(self.size/2) - self.pos[1])**2)
-
-        
-
 
     
 grid_points = grid(1000)
@@ -66,4 +76,6 @@ ax.legend()
 ax.set_title("RMS distance versus Number of steps")
 ax.set_xlabel("Number of steps")
 ax.set_ylabel("RMS distance")
+
+# plt.savefig("./polymers_random_walks/graph.png")
 plt.show()
